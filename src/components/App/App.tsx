968a5ddaa37241ae673d74268ddb6b7bcb81fe1f
MajoLkc <lukac.1majo@gmail.com>
2022-03-16 15:23:15 +0100

    Cosmetic code change in App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,12 @@
 import Invoice from "../Invoice"
 import PdfContext from "../PdfContext"
 
-const App: React.FC = () => {
-  return (
-    <>
-      <PdfContext>
-        <Invoice />
-      </PdfContext>
-    </>
-  )
-}
+const App: React.FC = () => (
+  <>
+    <PdfContext>
+      <Invoice />
+    </PdfContext>
+  </>
+)
 
 export default App
